f37fcdf9ef252552e57cbd94a084025518b4b850
Andriy Gordiyenko <andgordio@gmail.com>
2023-08-07 12:21:21 -0700

    Create WhatsNewHostingView.swift

---
+++ Sources/Appeal/What’s New/WhatsNewHostingView.swift
@@ -0,0 +1,15 @@
+//
+//  WhatsNewHostingView.swift
+//  
+//
+//  Created by Andriy Gordiyenko on 8/7/23.
+//
+
+import SwiftUI
+
+public struct WhatsNewHostingView: View {
+    public var body: some View {
+        EmptyView()
+            .withWhatsNew()
+    }
+}
